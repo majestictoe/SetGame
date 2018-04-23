da032670fa9bb4f7abd89375d36c2d47da6fb0fe
majestictoe <eshamble18@scienceleadership.org>
2018-04-23 08:56:53 -0400

    Initial commit

---
+++ app/src/main/java/cse/edu/eleanor/setgame/MainActivity.java
@@ -0,0 +1,30 @@
+package cse.edu.eleanor.setgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.content.pm.ActivityInfo;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        ImageButton[] cards = new ImageButton[0];
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        for(int j=0; j<3; j++) {
+            for (int k=0; k<6; k++) {
+                ImageButton myButton = new ImageButton(this);
+                cards[(6*j)+k] = myButton;
+                //android:id="@+id/buttonContainer"
+                //how to set image?
+                //myButton.();
+            }
+        }
+    }
+}
