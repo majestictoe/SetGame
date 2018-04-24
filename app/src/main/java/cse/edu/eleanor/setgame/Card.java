7525ae541a221946357a5694904ce5e054864754
majestictoe <eshamble18@scienceleadership.org>
2018-04-24 09:59:12 -0400

    deck created

--- app/src/main/java/cse/edu/eleanor/setgame/Card.java
+++ app/src/main/java/cse/edu/eleanor/setgame/Card.java
@@ -3,6 +3,14 @@ package cse.edu.eleanor.setgame;
 import android.widget.ImageButton;
 
 public class Card {
-    //Card(imageButton, color, number, etc){}
-
+    private int color;
+    private int number;
+    private int shape;
+    private int shading;
+    Card(int Color,int Number,int Shape, int Shading){
+        color = Color;
+        number = Number;
+        shape = Shape;
+        shading = Shading;
+    }
 }
--- app/src/main/java/cse/edu/eleanor/setgame/MainActivity.java
+++ app/src/main/java/cse/edu/eleanor/setgame/MainActivity.java
@@ -11,21 +11,20 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        ImageButton[] cards = new ImageButton[0];
+        Card[] cards = new Card[81];
+        for(int i =0; i<3; i++){
+            for(int j =0; j<3; j++){
+                for(int k =0; k<3; k++){
+                    for(int l =0; l<3; l++){
+                        cards[i*3+j*3+k*3+l] = new Card(i,j,k,l);
+                    }
+                }
+            }
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        for(int j=0; j<3; j++) {
-            for (int k=0; k<7; k++) {
-                //instead of myButton being an ImageButton, it should probably be an object that incorporates ImageButtons.
-                ImageButton myButton = new ImageButton(this);
-                cards[(7*j)+k] = myButton;
-                //android:id="@+id/buttonContainer"
-                //how to set image?
-                //myButton.();
-            }
-        }
     }
 }
