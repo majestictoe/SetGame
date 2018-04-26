247c415e259687384ff2108d0cef8518df3ea02a
majestictoe <eshamble18@scienceleadership.org>
2018-04-26 15:02:54 -0400

    fvkdsjrvckjaesvjkcv jfes

--- app/src/main/java/cse/edu/eleanor/setgame/MainActivity.java
+++ app/src/main/java/cse/edu/eleanor/setgame/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
         protected void onCreate(Bundle savedInstanceState) {
             Card[] cards = new Card[81];
+            int x = 0;
             for(int i =0; i<3; i++){
                 for(int j =0; j<3; j++){
                     for(int k =0; k<3; k++){
